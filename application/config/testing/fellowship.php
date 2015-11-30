57c3d1c073d52e4011bb7005645585a33ef15483
shenzheniapps <zhen@iappsasia.com>
2015-11-30 15:23:26 +0800

    update

--- application/config/testing/fellowship.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-	// $config['spark_api_base_url_v1']  = "http://192.168.1.167/Sparks/sparks-api/index.php/";
-	// echo "cesis";
-
-	$config['tq_api_base_url_v1']  = "http://localhost/church/tq_api/index.php/";
-?>
