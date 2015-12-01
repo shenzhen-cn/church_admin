772ad858dee1cbbbb634a806257d65093ca9c012
shenzheniapps <zhen@iappsasia.com>
2015-12-01 17:23:33 +0800

    pp

--- application/views/tq_head.php
+++ application/views/tq_head.php
@@ -7,7 +7,7 @@
 <link rel="shortcut icon" href="<?php echo base_url(); ?>public/images/favicon.png" />
 <!-- Bootstrap 3.3.5 -->
 <link href="<?php echo base_url(); ?>public/css/bootstrap.min.css" rel="stylesheet">
-<link href="<?php echo base_url(); ?>public/css/bootstrap.css.map" rel="stylesheet">
+<link href="<?php echo base_url(); ?>public/css/bootstrap.css.map" rel="stylesheet/map">
 <!-- Ionicons -->
 <link href="<?php echo base_url(); ?>public/plugins/css/ionicons.css" rel="stylesheet" />
 <!-- Ionicons -->
